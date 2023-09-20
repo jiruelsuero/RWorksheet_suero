6b5f6d2652e7fb92984913ac9482145dce373f40
jiruelsuero <jiruel.suero@students.isatu.edu.ph>
2023-09-20 04:08:56 +0000

    Rw

---
+++ RWorksheet_Suero#1.R
@@ -0,0 +1,27 @@
+#vector
+#age 34, 28, 22, 36, 27, 18, 52, 39, 42, 29
+#35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41
+#51, 35, 24, 33, 41
+
+#1
+  #a How many data points
+age <- c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,
+51, 35, 24, 33, 41)
+age
+#1.a
+  datap<- length(age)
+  datap  
+#1.b
+  
+#2Find the reciprocal of the values for age.Write the R code and its output.
+  reciprocated<-1/age
+  reciprocated  
+#3 Assign also new_age<- c (age,0, age).  What happen to the new _age?
+  new_age<- c(age, 0, age)
+  new_age
+#4Sort the values for age.Write the R code and its output.
+  sorted <-sort(age)
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+  
+   
